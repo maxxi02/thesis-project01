8a86b74155c9d44456c81b3cba6eb74c92ab6554
aeronphilip23-max <m44156529@gmail.com>
2025-10-21 02:31:51 +0800

    nextcnfig

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["philnews.ph", "alchetron.com", "calizamar.com"],
   },
+  output: "standalone",
+  swcMinify: true,
+  compress: true,
+  poweredByHeader: false,
 };
 
 export default nextConfig;
